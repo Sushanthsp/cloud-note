09574a2fde4e9a32b2612aa528c118b668bc744f
Sushanthsp <sushanthspoojary777@gmail.com>
2022-03-04 15:16:22 +0530

    init

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { ThemeContext } from "../context/notes/ThemeContext";
 import { BlogContext } from "../context/notes/BlogContext";
 import BlogItem from "./BlogItem";
