dbd34ab2d60ee860e4f2adfe140565157ed0b637
Sushanthsp <85091881+Sushanthsp@users.noreply.github.com>
2022-07-17 11:14:14 +0530

    Update noteState.js

--- src/context/notes/noteState.js
+++ src/context/notes/noteState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { NoteContext } from "./noteContext";
  
 const NoteState = (props) => {
-  const host = "https://cloudsnotess.herokuapp.com";
+  const host = "http://localhost:5000";
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
  
